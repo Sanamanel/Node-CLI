fa4d74fff1387156d2f6976fc5c6c8aa878f7421
User <rachida.haddouch.webdev@gmail.com>
2021-01-25 12:02:48 +0100

    first push for CLI

---
+++ bin/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// Definitions of modules constants
+const axios = require("axios");
+const countryList = require("country-list");
+const chalk = require("chalk");
+const ora = require("ora");
+var figlet = require("figlet");
